a4a4c24ba6a1a708922e723a542bb8c6aa6e73ce
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-05-04 11:07:53 -0700

    center div hha

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <Image src="/qr.png" alt="QR Code" width={1000} height={1000} />
+    <div className="flex justify-center">
+      <Image
+        className="w-6/12 p-5"
+        width={500}
+        height={500}
+        src="/qr.png"
+        alt="QR Code"
+      />
     </div>
   );
 }
